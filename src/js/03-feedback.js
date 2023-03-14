61d2de3a6b939b5436e4ba47b7bdd6bab6973438
MarCher21 <chernykhmaryna@gmail.com>
2023-03-14 21:13:44 +0200

    update js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import { throttle } from 'lodash';
 
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input');
+const message = document.querySelector('textarea');
+const STORAGE_KEY = 'feedback-form-state';
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+populateForm();
+
+
+function onFormInput() {
+  const formData = {
+    email: email.value,
+    message: message.value,
+  };
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  e.currentTarget.reset();
+  console.log(localStorage.getItem(STORAGE_KEY));
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+
+function populateForm() {
+  const saveData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+  if (saveData === null) {
+    return;
+  }
+  email.value = saveData.email;
+  message.value = saveData.message;
+}
